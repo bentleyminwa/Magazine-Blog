6499b8ea92a7d12cf22a7187e262206c0341d5fb
Bentley Minwa <bentleyminwa@gmail.com>
2025-03-11 18:43:07 +0300

    feat: enhance Navbar links with active state styling; add clsx for conditional classes

--- components/layout/Navbar/nav-links.tsx
+++ components/layout/Navbar/nav-links.tsx
@@ -1,12 +1,27 @@
+"use client";
+
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
+
 import { navItems } from "@/config/data";
 import Link from "next/link";
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <ul className="flex items-center gap-8 uppercase text-xs font-semibold">
       {navItems.map((navItem) => (
         <li key={navItem.id}>
-          <Link href={navItem.href} className="tracking-wider">
+          <Link
+            href={navItem.href}
+            className={clsx(
+              "tracking-wider pb-2",
+              pathname === navItem.href
+                ? "text-red-600 border-b-2 border-red-500"
+                : ""
+            )}
+          >
             {navItem.label}
           </Link>
         </li>
