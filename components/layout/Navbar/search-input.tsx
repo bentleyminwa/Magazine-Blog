8ad3fd048b7f66ddeab520d4508c46d2126adbcf
Bentley Minwa <bentleyminwa@gmail.com>
2025-03-12 09:09:00 +0300

    feat: enhance SearchInput component with styling and icon; update placeholder text

--- components/layout/Navbar/search-input.tsx
+++ components/layout/Navbar/search-input.tsx
@@ -1,8 +1,17 @@
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+
 export default function SearchInput() {
   return (
-    <form className="">
-      <label htmlFor="search"></label>
-      <input id="search" name="search" placeholder="search for post" />
+    <form className="flex flex-row-reverse items-center border border-gray-400 rounded-full py-1 px-4 focus-within:border-white">
+      <label htmlFor="search">
+        <MagnifyingGlassIcon width={25} className="text-gray-400" />
+      </label>
+      <input
+        id="search"
+        name="search"
+        placeholder="search for news"
+        className="text-sm text-gray-300 focus:outline-none focus:ring-0"
+      />
     </form>
   );
 }
